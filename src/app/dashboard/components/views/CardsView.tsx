1b3a46c14135a671e9faf3dac32079d0388f2fec
wwharton <wwharton@umich.edu>
2024-12-21 18:33:41 -0800

    fixed forced styling

--- src/app/dashboard/components/views/CardsView.tsx
+++ src/app/dashboard/components/views/CardsView.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { useDataContext } from "@/app/dashboard/providers/DataProvider";
-import CardComponent from "@/app/dashboard/components/CardComponent";
+import Card from "@/app/dashboard/components/CardComponent";
 import { useDialogContext } from "@/app/dashboard/providers/DialogProvider";
 
 const CardsView: React.FC = () => {
@@ -10,17 +10,17 @@ const CardsView: React.FC = () => {
   const { openDialog } = useDialogContext();
 
   return (
-    <div className="p-6 bg-background text-foreground">
+    <div className="p-6 bg-background">
         <h1 className="text-3xl font-bold mb-6">All Cards</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="light grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {cards.map((card) => (
-            <CardComponent 
-                key={card.id} 
-                title={card.title} 
-                content={card.content} 
-                onEdit={() => openDialog("update", card)}
-                onDelete={() => openDialog("delete", card)}
-            />
+              <Card 
+                  key={card.id} 
+                  title={card.title} 
+                  content={card.content} 
+                  onEdit={() => openDialog("update", card)}
+                  onDelete={() => openDialog("delete", card)}
+              />
             ))}
         </div>
     </div>
