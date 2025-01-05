ebc39c326c2b11ec99397e67fbaa75bd1061d4e7
wwharton <wwharton@umich.edu>
2025-01-05 14:57:17 -0800

    rename temp for troubleshooting

--- src/app/dashboard/temp.tsx
+++ src/app/dashboard/temp.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DashboardClientLayout from "./DashboardClientLayout";
 
+// must update the casing of layout, somehow desynced in github from lower (local) upper (github)
 export default function DashboardLayout({
   children,
 }: {
